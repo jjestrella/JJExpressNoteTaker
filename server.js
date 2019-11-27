7b2e3da3c5da869d86ea96c89a444cdbee1ccbc0
jjestrella <estrella.jjuan@gmail.com>
2019-11-26 21:32:37 -0800

    added button

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoute = require("./routes/apiRoute");
+const htmlRoute = require("./routes/htmlRoute");
 
 // Initialize the app and create a port
 const app = express();
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use("/api", apiRoute);
+app.use("/", htmlRoute);
 
 app.listen(PORT, function() {
     console.log("Listening on PORT: " + PORT);
